0b46cb04364615e8643b4ce7a462ddfa9cb91f60
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2020-11-21 15:20:55 +0100

    Reproducing the issue
    Severity	Code	Description	Project	File	Line	Suppression State
    Error	MSB4018	The "TestTask" task failed unexpectedly.
    System.IO.FileNotFoundException: Could not load file or assembly 'System.Runtime, Version=5.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a' or one of its dependencies. The system cannot find the file specified.
    File name: 'System.Runtime, Version=5.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
       at CustomTask.TestTask.Execute()
       at Microsoft.Build.BackEnd.TaskExecutionHost.Microsoft.Build.BackEnd.ITaskExecutionHost.Execute()
       at Microsoft.Build.BackEnd.TaskBuilder.<ExecuteInstantiatedTask>d__26.MoveNext()
    
    WRN: Assembly binding logging is turned OFF.
    To enable assembly bind failure logging, set the registry value [HKLM\Software\Microsoft\Fusion!EnableLog] (DWORD) to 1.
    Note: There is some performance penalty associated with assembly bind failure logging.
    To turn this feature off, remove the registry value [HKLM\Software\Microsoft\Fusion!EnableLog].
    	TaskConsumer	E:\projects\custom-msbuild-task\TaskConsumer\TaskConsumer.csproj	13

---
+++ CustomTask/TestTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace CustomTask
+{
+    public class TestTask : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Hello from the Custom MsBuild Task!");
+            return true;
+        }
+    }
+}
